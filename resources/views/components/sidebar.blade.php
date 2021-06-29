081685265865d028a2a1acf4f630e89ef1d4fff2
sourovahmad <mdsourovahmad19@gmail.com>
2021-06-29 16:28:11 +0600

    userControllers

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -17,6 +17,23 @@
     {{-- *********************************Super Admin**********************************  --}}
 
 
+
+    <hr class="sidebar-divider m-1 p-0 ">
+
+@if($auth->role_id == 1)
+
+
+    <li class="nav-item active ">
+        <a class="nav-link p-3 " href="{{ route('admin.users.index') }}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>{{__('Dashboard')}}</span></a>
+    </li>
+
+@endif
+
+
+
+
     <hr class="sidebar-divider m-1 p-0 ">
 
 @if($auth->role_id == 1 || $auth->role_id == 2)
