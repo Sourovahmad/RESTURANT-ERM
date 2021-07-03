c3ac1e1c5af0ce1c7345ec04b14cc62cf853d73e
sourovahmad <mdsourovahmad19@gmail.com>
2021-07-03 22:57:26 +0600

    migration for product

--- database/migrations/2021_07_03_220510_create_products_table.php
+++ database/migrations/2021_07_03_220510_create_products_table.php
@@ -16,7 +16,9 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedBigInteger('name');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('image_id');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
