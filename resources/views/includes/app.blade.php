839146c0f1749e2d32fb8fa575f91b33b4b10a98
sourovahmad <mdsourovahmad19@gamil.com>
2021-08-28 09:14:22 +0600

    hello

--- resources/views/includes/app.blade.php
+++ resources/views/includes/app.blade.php
@@ -20,6 +20,10 @@
         @yield('customCSS')
 
 
+
+
+
+
         <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
         <script src="{{ asset('js/popper.min.js') }}"></script>
         <script src="{{ asset('js/bootstrap.min.js') }}"></script>
