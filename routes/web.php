208894013996f43cb2298f5743de256b2c899ab3
sourovahmad <mdsourovahmad19@gamil.com>
2021-08-28 10:07:26 +0600

    dd

--- routes/web.php
+++ routes/web.php
@@ -71,6 +71,9 @@ Route::middleware(['auth:sanctum'])->group(function(){
 
 
 
+
+    
+
     // printer controllers
         Route::post('/print-queue', [PrintQueueController::class, 'store'])->name('print-queue');
     Route::get('/print-memo', [PrintQueueController::class, 'index'])->name('print-memo');
