aff5f26e7d967fe31c989897590dd9b6fb06fb8b
sourovahmad <mdsourovahmad19@gmail.com>
2021-09-01 22:12:30 +0600

    time testing

--- resources/views/admin/table/index.blade.php
+++ resources/views/admin/table/index.blade.php
@@ -253,7 +253,7 @@
             <div class="modal-content">
                 <div class="modal-header bg-techbot-dark">
                     <h5 class="modal-title " id="edit-modal-label ">
-                        Edit Printer</h5>
+                        Edit Table</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                             aria-hidden="true">&times;</span>
                     </button>
